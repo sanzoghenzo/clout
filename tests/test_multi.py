0ef9e56b46ca7eca8106fcbbab66645c62a1bb62
desert <desert@cordaz.com>
2019-08-29 18:44:46 +0000

    Test appconfig.py

--- tests/test_multi.py
+++ tests/test_multi.py
@@ -2,6 +2,7 @@ import desert.loaders.multi
 
 
 def test_deep_chainmap():
+    """Get values from the nth dict even if the first dict has the first key."""
     maps = [{"a": {}}, {"a": {"b": {}}}, {"a": {"b": {"c": 1337}}}]
     dcm = desert.loaders.multi.DeepChainMap(*maps)
     assert dcm["a"]["b"]["c"] == 1337
---
+++ tests/test_system.py
@@ -0,0 +1,49 @@
+import subprocess
+import sys
+
+import importlib_resources
+import tests.examples
+
+
+def test_appconfig(tmp_path):
+    """Run the appconfig example.
+
+    Get data from CLI, envvars, and config file.
+
+    """
+    with importlib_resources.path(tests.examples, "appconfig.py") as path:
+        config_path = tmp_path / "config.toml"
+        config_path.write_text(
+            """\
+        [dance]
+        logging = true
+        priority = 3
+        """
+        )
+
+        env = {"MYAPP_APP_DANCE_DRY_RUN": "1", "TEST_CONFIG_PATH": config_path}
+        args = [
+            "myapp",
+            "dance",
+            "--debug",
+            "db",
+            "--host",
+            "example.com",
+            "--port",
+            "9999",
+            "user",
+            "--name",
+            "Alice",
+            "user",
+            "--name",
+            "Bob",
+        ]
+
+        proc = subprocess.run(
+            [sys.executable, path] + args, capture_output=True, check=False, env=env
+        )
+        assert proc.returncode == 0, proc.stderr.decode()
+
+        output = proc.stdout.decode()
+        expected = "Dancing with config:\n Config(db=DB(host='example.com', port=9999, user=User(name='Alice')), debug=True, user=User(name='Bob'), priority=3.0, logging=True, dry_run=True)\n"
+        assert output == expected
