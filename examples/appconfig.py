65817466084f79beb7eaed692996dcfe1d20cb80
akb <akb+desert@cordaz.com>
2019-08-29 18:11:36 +0000

    Remove unused imports

--- examples/appconfig.py
+++ examples/appconfig.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import dataclasses
 import os
 import pathlib
-import typing as t
 
 import attr
-import click
 
 import desert.encoders.toml
 import desert.loaders.appfile
