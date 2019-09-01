19ec7fa4e52a096d95fd819f2e718212752f1ea4
desert <desert@cordaz.com>
2019-09-01 04:54:45 +0000

    Add comments

--- tests/examples/minicli.py
+++ tests/examples/minicli.py
@@ -49,17 +49,25 @@ CONFIG_FILE_PATH = pathlib.Path(appdirs.user_config_dir(APP_NAME)) / "config.tom
 CONFIG_FILE_DATA = toml.loads(CONFIG_FILE_PATH.read_text())
 
 # Read from environment_variables prefixed `MYAPP_CONFIG_`.
+# XXX How to make this simpler?
+# Provide a `prefix=` argument? If it's `desert.load_env(Config, prefix=)`, then how to
+# provide the app name and config name separately? Is that useful?
 ENVVAR_DATA = desert.loaders.env.Env(app_name=APP_NAME).prep(Config, name=CONFIG_NAME)
 
 # Combine config file and envvars to set CLI defaults.
+# XXX make a function `desert.combine()`?
 CONTEXT_SETTINGS = dict(
     default_map=desert.loaders.multi.DeepChainMap(ENVVAR_DATA, CONFIG_FILE_DATA)
 )
 
 # Define the CLI.
+# XXX Should it just be called `desert.Command()`?
 commands = [
     desert.loaders.cli.DesertCommand(
-        "run", type=Config, context_settings=CONTEXT_SETTINGS
+        "run",
+        type=Config,
+        context_settings=CONTEXT_SETTINGS,
+        help="Run the app with given configuration object.",
     )
 ]
 cli = click.Group(commands={c.name: c for c in commands})
